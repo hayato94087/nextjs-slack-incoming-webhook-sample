be9c20524cb305808b5fde75bad347909b9a50ef
hayato94087 <hayato94087@gmail.com>
2023-07-29 06:42:49 +0900

    ボタンをクリックするとSlackのチャンネルに通知機能を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Button from "@/components/button";
+
 export default function Home() {
   return (
     <main className="text-lg">
-      テストページ
+      <Button message="Hello World" />
+      <Button message="Hello Universe" />
     </main>
   )
 }
---
+++ src/components/button.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { sendSlackMessage } from "@/server-actions/send-slack-message-action";
+import { FC, useTransition } from "react";
+
+interface ButtonProps {
+  message: string;
+}
+
+const Button:FC<ButtonProps> = (props) => {
+  let [isPending, startTransition] = useTransition();
+  return (
+    <>
+      {isPending ? (
+        <button onClick={() => startTransition(() => sendSlackMessage({message:`${props.message}`}))} className="border-2 py-2 px-3 bg-slate-400 text-white rounded-lg w-[250px]" disabled={true}>
+        {props.message} を送信中
+        </button>
+      ) : (
+        <button onClick={() => startTransition(() => sendSlackMessage({message:`${props.message}`}))} className="border-2 py-2 px-3 bg-slate-800 text-white rounded-lg w-[250px]">
+          {props.message} を送信
+        </button>
+      )}
+    </>
+  );
+};
+
+export default Button;
